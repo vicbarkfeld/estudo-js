63284d626a6f1b3735db0feb631efb4627745792
Victoria Barkfeld <victoriacbarkfeld@gmail.com>
2021-07-08 16:40:06 -0300

    Meu primeiro programa - trabalhando com variaveis

---
+++ 1-meuPrimeiroPrograma.js
@@ -0,0 +1,17 @@
+console.log("Meu Primeiro programa: trabalhando com Variaveis");
+ 
+const idade =24;
+
+console.log("idade");
+console.log(idade);
+console.log(idade+2);
+console.log(idade-2);
+console.log(idade/2);
+
+const idadeSomada = idade+2;
+console.log(idadeSomada);
+ 
+
+// No exemplo abaixo: Quando colocamos algo entre aspas o interpretador do JS entende aquilo como sendo um texto literal. 
+// const nome = "Lais";
+// console.log("nome"); 

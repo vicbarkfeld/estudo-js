98748bbfb341424e43d7f2a64c333d574690fbc6
Victoria Barkfeld <victoriacbarkfeld@gmail.com>
2021-07-08 20:39:29 -0300

    aula 5

---
+++ 5-atribuicaoVariaveis.js
@@ -0,0 +1,12 @@
+console.log("Trabalhando com atribuição de variaveis");
+
+const idade =29;
+let nome = "Victória";
+const sobrenome = "Barkfeld";
+
+// console.log(nome + " " + sobrenome)
+console.log(nome, sobrenome); 
+console.log(`Meu nome é ${nome} ${sobrenome}`);
+
+nome = nome + sobrenome;
+console.log(nome);
